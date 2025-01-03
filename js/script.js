19daa6a515aab6adf847424281e73873cdfbb38d
alexssandro772 <alex.araujo77@gmail.com>
2025-01-02 22:13:34 -0300

    js em pergutas no site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,56 @@
-const links = document.querySelectorAll('.header-menu a');
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
 
-console.log(links);
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+
+// Ativar itens do orçamento
+
+function ativarProtudo(parametro){
+  const elemento = document.getElementById(parametro);
+  if(elemento){
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+const parametros = new URLSearchParams(location.search);
+
+parametros.forEach(ativarProtudo);
+
+// let busca = parametros.search;
+// console.log(parametros);
+
+
+
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  // console.log(ativa);
+  pergunta.setAttribute('aria-expanded', ativa);
+  // console.log(resposta);
+
+}
+function eventosPerguntas(pergunta){
+  // console.log(pergunta);
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+
+perguntas.forEach(eventosPerguntas);
