db653eb5abc8f19e9fe92ca57e61e30a1cf9896f
alexssandro772 <alex.araujo77@gmail.com>
2025-01-04 10:43:07 -0300

    js na galeria de bicicletas

--- js/script.js
+++ js/script.js
@@ -54,3 +54,32 @@ function eventosPerguntas(pergunta){
 
 
 perguntas.forEach(eventosPerguntas);
+
+
+// Galeria de Bicicletas
+
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+// console.log(galeria, galeriaContainer);
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = window.matchMedia('(min-width: 1000px)').matches;
+  console.log(media);
+  if(media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação do site
+if(window.SimpleAnime){
+  new  SimpleAnime();
+}
